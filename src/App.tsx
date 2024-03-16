696461fb15e45d776bb00c9589f141b32090a19c
dangthaii <dangthaiuseful@gmail.com>
2024-03-16 15:04:05 +0700

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1 className="text-4xl">Hello World</h1>
+      <p>My first React app</p>
+    </div>
+  );
 }
 
 export default App;
